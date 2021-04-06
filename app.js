a9e494d4c5fa89b687d1661fc9d1f758e177a4d7
f-westergren <folke.westergren@gmail.com>
2021-04-05 21:43:52 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+
+  console.log("APP IS RUNNING!");
+})();
